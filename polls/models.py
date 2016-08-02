5f18c4505d0e7f6ff29bddfbe257baa126e4b532
Abhishek Sajwan <abhishek.sajwan97@gmail.com>
2016-08-03 00:00:01 +0530

    1st project done

--- polls/models.py
+++ polls/models.py
@@ -10,7 +10,7 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelat(days=1)
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
