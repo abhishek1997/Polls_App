0b1ba541ce91570730f3161c65f955cfb7c5a68e
Abhishek Sajwan <abhishek.sajwan97@gmail.com>
2016-08-05 17:03:05 +0530

    tut 3 completed

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from django.http import HttpResponse
+#from django.template import RequestContext, loader
+from polls.models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    '''template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {'latest_question_list': latest_question_list,})'''
+    context = {'latest_question_list': latest_question_list}
+    '''output = ', '.join([p.question_text for p in latest_question_list])
+    return HttpResponse(template.render(context))'''
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    retrun HttpResponse("You're looking at question %s." % question_id)
+    '''try:
+        question = Questio.objects.get(pk = question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")'''
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/detail.html', {question : 'question'})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
